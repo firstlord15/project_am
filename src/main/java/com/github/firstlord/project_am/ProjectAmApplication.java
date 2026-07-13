b5f143ff1b5752ff1802bbc24eee14dc5efe2573
firstlord15 <ratmir.yuldashe28@mail.ru>
2026-07-14 00:16:20 +0300

    first commit

---
+++ src/main/java/com/github/firstlord/project_am/ProjectAmApplication.java
@@ -0,0 +1,11 @@
+package com.github.firstlord.project_am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAmApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/firstlord/project_am/ProjectAmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.firstlord.project_am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
