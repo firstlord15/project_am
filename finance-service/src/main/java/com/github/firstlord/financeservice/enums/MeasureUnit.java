6894c683b01e651eee58fb93cd4915539cc10f7c
firstlord15 <ratmir.yuldashe28@mail.ru>
2026-07-17 01:47:14 +0300

    feat(finance): add FinancialRecord entity and MeasureUnit enum

---
+++ finance-service/src/main/java/com/github/firstlord/financeservice/enums/MeasureUnit.java
@@ -0,0 +1,10 @@
+package com.github.firstlord.financeservice.enums;
+
+public enum MeasureUnit {
+    PIECE,    // шт
+    PORTION,  // порц
+    KG,       // кг
+    GRAM,     // г
+    LITER,    // л
+    ML        // мл
+}
